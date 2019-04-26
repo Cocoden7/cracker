f4976ead4db6ba2d16db12faca4abeef7d1affe1
Jerome <jerome.delforge@student.uclouvain.be>
2019-04-26 15:13:09 +0200

    gestion des args

--- cracker.c
+++ cracker.c
@@ -1,5 +1,27 @@
+
+int NTHREAD = 1;
+bool CONS = false;
+bool FILEOUT = false;
+
 int main(int argc, char const *argv[])
 {
+	for (int i = 0; i < argc || i < 5; ++i)
+	{
+		if (strcmp(argv[i],"-t"))
+		{
+			NTHREAD = atoi(argv[i+1])
+		}
+		if (strcmp(argv[i],"-c"))
+		{
+			CONS = true;
+		}
+		if (strcmp(argv[i],"-o"))
+		{
+			FILEOUT = true;
+		}
+	}
+
 	
+
 	return 0;
 }
