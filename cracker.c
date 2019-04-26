8b0ec905de66df32fce07ade7b278fe2a6a96630
Jerome <jerome.delforge@student.uclouvain.be>
2019-04-26 15:45:39 +0200

    fonction readfile

--- cracker.c
+++ cracker.c
@@ -1,7 +1,10 @@
+#include <stdlib.h>
+#include <stdio.h>
 
 int NTHREAD = 1;
 bool CONS = false;
 bool FILEOUT = false;
+
 
 int main(int argc, char const *argv[])
 {
@@ -21,7 +24,26 @@ int main(int argc, char const *argv[])
 		}
 	}
 
-	
+
 
 	return 0;
+}
+
+public void readfile(const char *filename){
+
+	FILE* file = fopen(filename, "r");
+	if (file == NULL)
+	{
+		printf("Impossible d'ouvrir le fichier %s. \n", filename);
+	}
+	struct stat *buf = malloc(sizeof(struct stat));
+	fstat(file, buf);
+	off_t size = buf->st_size;
+	int sizetab = (int) size/32;
+	char *tab1[sizetab];
+	for (int i = 0; i < sizetab; ++i)
+	{
+		fgets(tab1[i],32,file);
+	}
+
 }
